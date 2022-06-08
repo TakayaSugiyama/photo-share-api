af37838e538f306e35414a97ac60c0da2b509164
takayasugiyama <s.takaya1027@gmail.com>
2022-06-09 08:36:13 +0900

    1 vs 多

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const { ApolloServer, gql } = require("apollo-server");
 
 const typeDefs = gql`
+  type User {
+    githubLogin: ID!
+    name: String
+    avatar: String
+    postedPhotos: [Photo!]!
+  }
+
+  enum PhotoCategory {
+    SELFIE
+    PORTRAIT
+    ACTION
+    LANDSCAPE
+    GRAPHIC
+  }
+
   type Photo {
     id: ID!
     url: String!
     name: String!
     description: String
+    category: PhotoCategory!
+    photoBy: User!
   }
 
   type Query {
@@ -13,13 +30,55 @@ const typeDefs = gql`
     allPhotos: [Photo!]!
   }
 
+  input PostPhotoInput {
+    name: String!
+    description: String
+    category: PhotoCategory = PORTRAIT
+  }
+
   type Mutation {
-    postPhoto(name: String!, description: String): Photo!
+    postPhoto(input: PostPhotoInput!): Photo!
   }
 `;
 
-const photos = [];
 let _id = 0;
+const users = [
+  {
+    githubLogin: "mHattrup",
+    name: "Mike Hattrup",
+  },
+  {
+    githubLogin: "gPlake",
+    name: "Glen Plake",
+  },
+  {
+    githubLogin: "sSchmidt",
+    name: "Scot Schmidt",
+  },
+];
+
+var photos = [
+  {
+    id: "1",
+    name: "Dropping the Heart Chute",
+    description: "The heart chute is one of my favorite chutes",
+    category: "ACTION",
+    githubUser: "gPlake",
+  },
+  {
+    id: "2",
+    name: "Enjoying the sunshine",
+    category: "SELFIE",
+    githubUser: "sSchmidt",
+  },
+  {
+    id: "3",
+    name: "Gunbarrel 25",
+    description: "25 laps on gunbarrel today",
+    category: "LANDSCAPE",
+    githubUser: "sSchmidt",
+  },
+];
 
 const resolvers = {
   Query: {
@@ -30,7 +89,7 @@ const resolvers = {
     postPhoto: (parent, args) => {
       let newPhoto = {
         id: _id++,
-        ...args,
+        ...args.input,
       };
       photos.push(newPhoto);
       return newPhoto;
@@ -38,6 +97,11 @@ const resolvers = {
   },
   Photo: {
     url: (parent) => `https://yoursite.com/img/${parent.id}.jpg`,
+    photoBy: (parent) => users.find((u) => u.githubLogin == parent.githubUser),
+  },
+  User: {
+    postedPhotos: (parent) =>
+      photos.filter((p) => p.githubUser == parent.githubLogin),
   },
 };
 
