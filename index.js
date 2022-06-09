590c115eb1f63b282abb6a1ab8411d201e3f79a7
takayasugiyama <s.takaya1027@gmail.com>
2022-06-09 23:47:56 +0900

    connect mongodb

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-const { ApolloServer, gql } = require("apollo-server-express");
+const { ApolloServer } = require("apollo-server-express");
 const express = require("express");
-const { GraphQLScalarType, Kind } = require("graphql");
 const expressPlayGround =
   require("graphql-playground-middleware-express").default;
 const { readFileSync } = require("fs");
-
-const app = express();
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
 const typeDefs = readFileSync("./type.graphql", "UTF-8");
 const resolvers = require("./resolvers");
 
 const startServer = async () => {
+  const app = express();
+  const MONGO_DB = process.env.DB_HOST;
+  const client = new MongoClient(MONGO_DB, { useNewUrlParser: true });
+  await client.connect();
+  const db = client.db();
+  const context = { db };
+  console.log("Connected successfully to server");
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    context,
     csrfPrevention: true,
   });
 
