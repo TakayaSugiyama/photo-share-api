7f8760b08ab7aae66f60b683af6fa28d79a45a2f
takayasugiyama <s.takaya1027@gmail.com>
2022-06-09 21:36:07 +0900

    多 対 多

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const typeDefs = gql`
     name: String
     avatar: String
     postedPhotos: [Photo!]!
+    inPhotos: [Photo!]!
   }
 
   enum PhotoCategory {
@@ -23,6 +24,7 @@ const typeDefs = gql`
     description: String
     category: PhotoCategory!
     photoBy: User!
+    taggedUsers: [User!]!
   }
 
   type Query {
@@ -80,6 +82,13 @@ var photos = [
   },
 ];
 
+var tags = [
+  { photoID: "1", userID: "gPlake" },
+  { photoID: "2", userID: "sSchmidt" },
+  { photoID: "2", userID: "mHattrup" },
+  { photoID: "2", userID: "gPlake" },
+];
+
 const resolvers = {
   Query: {
     totalPhotos: () => photos.length,
@@ -97,11 +106,21 @@ const resolvers = {
   },
   Photo: {
     url: (parent) => `https://yoursite.com/img/${parent.id}.jpg`,
-    photoBy: (parent) => users.find((u) => u.githubLogin == parent.githubUser),
+    photoBy: (parent) => users.find((u) => u.githubLogin === parent.githubUser),
+    taggedUsers: (parent) =>
+      tags
+        .filter((tag) => tag.photoID === parent.id)
+        .map((tag) => tag.userID)
+        .map((userID) => users.find((u) => u.githubLogin === userID)),
   },
   User: {
     postedPhotos: (parent) =>
       photos.filter((p) => p.githubUser == parent.githubLogin),
+    inPhotos: (parent) =>
+      tags
+        .filter((tag) => tag.userID === parent.ID)
+        .map((tag) => tag.photoID)
+        .map((photoID) => photos.find((p) => p.ID === photoID)),
   },
 };
 
