48137df2f3ef8e3e44022d264d3d1637eb773f43
takayasugiyama <s.takaya1027@gmail.com>
2022-06-12 06:17:25 +0900

    enable subscription

--- index.js
+++ index.js
@@ -5,33 +5,58 @@ const expressPlayGround =
 const { readFileSync } = require("fs");
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
+const { WebSocketServer } = require("ws");
+const { useServer } = require("graphql-ws/lib/use/ws");
+const { createServer } = require("http");
+const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
 
 const typeDefs = readFileSync("./type.graphql", "UTF-8");
 const resolvers = require("./resolvers");
 
 const startServer = async () => {
   const app = express();
+  const httpServer = createServer(app);
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: "/graphql",
+  });
+  const serverCleanup = useServer({ schema }, wsServer);
+
   const MONGO_DB = process.env.DB_HOST;
   const client = new MongoClient(MONGO_DB, { useNewUrlParser: true });
   await client.connect();
   const db = client.db();
   console.log("Connected successfully to server");
   const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+    schema,
     context: async ({ req }) => {
       const githubToken = req.headers.authorization;
       const currentUser = await db.collection("users").findOne({ githubToken });
       return { db, currentUser };
     },
     csrfPrevention: true,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose();
+            },
+          };
+        },
+      },
+    ],
   });
 
   await server.start();
   server.applyMiddleware({ app });
   app.get("/", (req, res) => res.end("Welcome to the PhotoShare API"));
   app.get("/playground", expressPlayGround({ endpoint: "/graphql" }));
-  app.listen({ port: 4000 }, () => console.log("server start!!"));
+  httpServer.listen({ port: 4000 }, () => console.log("server start!!"));
 };
 
 startServer();
